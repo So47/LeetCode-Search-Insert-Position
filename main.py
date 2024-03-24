5dc4da0d4990cc947032f10eac4fafc50a25a45c
Sondos Mohamed <39069270+So47@users.noreply.github.com>
2024-03-24 07:21:19 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low = 0
+        n = len(nums)
+        high = n - 1
+        while low <= high:
+            mid = (low+high)//2
+            guess = nums[mid]
+
+            if guess == target:
+                return mid
+            elif guess > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return low               
+        
